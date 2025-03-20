e1dc4915d202cf4c040081d76ee123244ce3fe99
Lucas Triozon <lucas.triozon@orange.fr>
2025-03-20 09:31:15 +0100

    ddd

---
+++ src/func.js
@@ -0,0 +1,98 @@
+let nbRecipes = 0
+let recipes = {}
+
+export async function getCityInfo(req, rep) {
+    try {
+        let cityId = req.params["cityId"]
+
+        const responseInsight = await fetch(`https://api-ugi2pflmha-ew.a.run.app/cities/${cityId}/insights?apiKey=${process.env.API_KEY}`);
+
+        if (!responseInsight.ok) {
+            throw new Error(`La ville n'existe pas`);
+        }
+        
+        const insights = await responseInsight.json();
+
+        const responseMeteo = await fetch(`https://api-ugi2pflmha-ew.a.run.app/weather-predictions?apiKey=${process.env.API_KEY}&cityId=${cityId}`);
+
+        if (!responseMeteo.ok) {
+            throw new Error(`Pas de météo ?`);
+        }
+        
+        const meteo = await responseMeteo.json();
+
+        rep.send({
+            coordinates:insights.coordinates,
+            population:insights.population,
+            knownFor:insights.knownFor,
+            weatherPredictions:meteo[0].predictions,
+            recipes:recipes[cityId] ? recipes[cityId] : []
+        })
+
+    } catch (error) {
+        console.error(error);
+        rep.status(500).send({ error: error.message });
+    }
+
+}
+
+
+
+
+export async function postCityRecipe(req, rep) {
+    let cityId = req.params["cityId"]
+    
+    try {
+        if (!req.body) {
+            throw new Error("No body");
+        }
+
+        let { content } = req.body
+
+        if (!content) {
+            throw new Error("No content");
+        }
+
+        const responseCity = await fetch(`https://api-ugi2pflmha-ew.a.run.app/cities?apiKey=${process.env.API_KEY}&search=${cityId}`);
+
+        if (responseCity.status == 404) {
+            throw new Error("La ville n'existe pas");
+        } 
+
+        if (!Object.keys(recipes).includes(cityId)) {
+            recipes[cityId] = []
+        }
+
+        if (content.length < 10 || content.length > 2000) {
+            throw new Error("Content too short or too long");
+        }
+
+        recipes[cityId].push({id: ++nbRecipes, content:content})
+        rep.send({id: nbRecipes, content:content})
+
+    } catch (error) {
+        console.error(error);
+        rep.status(500).send({ error: error.message });
+    }
+    
+}
+
+
+export async function deleteCityRecipe(req, rep) {
+    try {
+        let cityId = req.params["cityId"]
+        let recipeId = req.params["recipeId"]
+
+        const responseCity = await fetch(`https://api-ugi2pflmha-ew.a.run.app/cities?apiKey=${process.env.API_KEY}&search=${cityId}`);
+
+        if (responseCity.status == 404) {
+            throw new Error("La ville n'existe pas");
+        }
+    } catch (error) {
+        console.error(error);
+        rep.status(500).send({ error: error.message });
+    }
+    
+
+
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,46 @@
 import 'dotenv/config'
 import Fastify from 'fastify'
 import { submitForReview } from './submission.js'
+import { deleteCityRecipe, getCityInfo, postCityRecipe } from './func.js'
+import FastifySwagger from "@fastify/swagger";
+import FastifySwaggerUi from "@fastify/swagger-ui";
 
 const fastify = Fastify({
   logger: true,
 })
+
+await fastify.register(FastifySwagger, {
+    openapi: {
+      openapi: "3.0.0",
+      info: {
+        title: "Test swagger",
+        description: "Testing the Fastify swagger API",
+        version: "0.1.0",
+      },
+
+      components: {
+        securitySchemes: {
+          apiKey: {
+            type: "apiKey",
+            name: "apiKey",
+            in: "header",
+          },
+        },
+      },
+      externalDocs: {
+        url: "https://swagger.io",
+        description: "Find more info here",
+      },
+    },
+  });
+  
+  await fastify.register(FastifySwaggerUi, {
+    routePrefix: "/",
+    uiConfig: {
+      docExpansion: "full",
+      deepLinking: false,
+    },
+  });
 
 fastify.listen(
   {
@@ -24,3 +60,22 @@ fastify.listen(
     submitForReview(fastify)
   }
 )
+
+const schema = {
+    type: 'object',
+    required: ['content'],
+    additionalProperties: false,
+    properties: {
+        content: { type: 'string' }
+    }
+}
+
+fastify.get("/cities/:cityId/infos", getCityInfo)
+
+fastify.post("/cities/:cityId/recipes", {
+    schema: {
+      body: schema
+    }
+  }, postCityRecipe)
+
+fastify.delete("/cities/:cityId/recipes/:recipeId", deleteCityRecipe)
